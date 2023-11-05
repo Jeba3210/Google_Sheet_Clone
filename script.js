7c2aa2c30736f55e9f89368faad3ce5ec3414b50
Jeba3210 <137270674+Jeba3210@users.noreply.github.com>
2023-11-05 21:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,145 @@
-
 let columns = 26;
 let rows = 100;
-
 const headerContainer = document.querySelector(".header");
-const slNoContainer = document.querySelector(".sl_no");
+const serialNumbersContainer = document.querySelector(".sno");
 const mainContainer = document.querySelector(".main");
 
 
 function createHeaderCells() {
     for (let i = 0; i <= columns; i++) {
         const headerCell = document.createElement("div");
-        if(i !== 0){
-            headerCell.className = ("header_cell");
-            headerCell.innerText = String.fromCharCode(64+i);
-        }else{
-            headerCell.className = ("header_slcell");
+        headerCell.className = "header-cell cell";
+        if (i !== 0) {
+            headerCell.innerText = String.fromCharCode(64 + i);
         }
-        headerContainer.appendChild(headerCell);
-        
+        headerContainer.appendChild(headerCell)
     }
 }
 
-
-function createSlNo(){
-    for (let i = 1; i <= 100; i++) {
-        const slNoCell = document.createElement("div");
-        slNoCell.className = "slNo_cell";
-        slNoCell.innerText = i;
-
-        slNoContainer.appendChild(slNoCell);
-        
+function createSerialNumberCells() {
+    for (let i = 1; i <= rows; i++) {
+        const snoCell = document.createElement("div");
+        snoCell.innerText = i;
+        snoCell.className = "sno-cell cell"
+        serialNumbersContainer.appendChild(snoCell);
     }
 }
 
-function createrow(){
+function createRow(rowNumber) {
     const row = document.createElement("div");
-    for (let i = 1; i <= columns; i++) {
-      const cell = document.createElement("div");
-      cell.className = "main_cell";
-      row.appendChild(cell);
-      cell.contentEditable = true;
-    }
     row.className = "row";
+    for (let i = 1; i <= columns; i++) {
+        const cell = document.createElement("div");
+        cell.className = "main-cell cell";
+        cell.contentEditable = true;
+        row.appendChild(cell);
+
+        cell.id = String.fromCharCode(64 + i) + rowNumber;
+
+        cell.addEventListener("focus", onCellFocus);
+        cell.addEventListener("input", onFormChange);
+    }
     mainContainer.appendChild(row);
 }
 
-function mainSection(){
-    for (let i = 0; i < rows; i++) {
-       createrow();
-        
+function buildMainSection() {
+    for (let i = 1; i <= rows; i++) {
+        createRow(i);
     }
 }
 
 createHeaderCells();
-createSlNo();
-mainSection();
+createSerialNumberCells();
+
+buildMainSection();
+
+const cellNamePlaceholder = document.querySelector("#active-cell");
+const fontSizeInput = document.querySelector("#fontsize");
+const fontFamilyInput = document.querySelector("#fontfamily");
+const form = document.querySelector("#form")
+
+let activeElement = null;
+
+const state = {};
+
+const defaultProperties = {
+    fontFamily: 'sans',
+    fontSize: 16,
+    color: "#000000",
+    textAlign: "left", // "left", "center", "right"
+    backgroundColor: "#ffffff",
+    isBold: false,
+    isItalic: false,
+    isUnderlined: false,
+    value: ''
+}
+
+function onCellFocus(event) {
+    const elementId = event.target.id;
+    cellNamePlaceholder.innerText = elementId;
+    activeElement = event.target;
+    if (state[elementId]) {
+        resetOptions(state[elementId]);
+    }
+    else {
+        resetOptions(defaultProperties);
+    }
+}
+
+function resetOptions(optionsState) {
+   
+    form.fontfamily.value = optionsState.fontFamily;
+    form.fontsize.value = optionsState.fontSize;
+    form.textalign.value = optionsState.textAlign; // "right" | "left" | "center"
+    form.bold.checked = optionsState.isBold
+    form.italic.checked = optionsState.isItalic;
+    form.underlined.checked = optionsState.isUnderlined;
+    form.textcolor.value = optionsState.color;
+    form.bgcolor.value = optionsState.backgroundColor;
+}
+
+function onFormChange() {
+    if (!activeElement) {
+        alert("Please select a cell to make changes");
+        form.reset();
+        return;
+    }
+
+
+    let currentState = {
+        textColor: form.textcolor.value,
+        backgroundColor: form.bgcolor.value,
+        fontSize: form.fontsize.value,
+        fontFamily: form.fontfamily.value,
+        isBold: form.bold.checked,
+        isItalic: form.italic.checked,
+        isUnderlined: form.underlined.checked,
+        textAlign: form.textalign.value // "left" , "right" , "center"
+    }
+    applyStylesToCell(currentState);
+
+
+    state[activeElement.id] = { ...currentState, value: activeElement.innerText };
+}
+
+function applyStylesToCell(styleObject) {
+    activeElement.style.fontSize = `${styleObject.fontSize}px`;
+    activeElement.style.fontFamily = styleObject.fontFamily;
+    activeElement.style.color = styleObject.textColor;
+    activeElement.style.backgroundColor = styleObject.backgroundColor;
+    activeElement.style.textAlign = styleObject.textAlign;
+
+    activeElement.style.fontWeight = styleObject.isBold ? "bold" : "normal";
+    activeElement.style.fontStyle = styleObject.isItalic ? "italic" : "normal";
+    activeElement.style.textDecoration = styleObject.isUnderlined ? "underline" : "none";
+}
+
+function exportData() {
+    let fileData = JSON.stringify(state);
+    let blob = new Blob([fileData], { type: "application/json" })
+    let url = URL.createObjectURL(blob);
+    let link = document.createElement("a");
+    link.href = url;
+    link.download = "sheet.json";
+    link.click();
+}
